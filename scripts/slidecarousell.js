9c99514b690b9fc24030e3c1d6cb4860f257d4ed
Thomas Kildahl <69842791+ThomasK97@users.noreply.github.com>
2021-05-28 14:34:27 +0200

    last update

--- scripts/slidecarousell.js
+++ scripts/slidecarousell.js
@@ -1,29 +1,34 @@
+const url ="https://www.flowerp.online/wp-json/wp/v2/posts";
+const display = document.querySelector(".carousel-slider");
+const container = document.querySelector(".slider");
 
-var slideIndex = 1;
-showSlides(slideIndex);
 
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
 
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
 
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-}
+fetch(url)
+    .then(response => response.json())
+    .then(data => posts(data))
+    .catch(error => {
+      
+      display.innerHTML = "Page not found"
+    })
+   
+
+post = (posts) => {
+  console.log(posts);
+
+      
+      for (post of posts) {
+          
+        
+          let newPost = `
+          <div class "postAll">
+          <h3>${post.content}</h3>
+          </div>  
+         
+          `;
+          display.innerHTML += newPost;
+          
+        }
+      }
+    
