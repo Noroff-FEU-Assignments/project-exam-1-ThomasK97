94b9f752b2aac86e9ced28b9e2bd2497dbfed6ef
Thomas Kildahl <69842791+ThomasK97@users.noreply.github.com>
2021-05-29 21:13:12 +0200

    last update

--- scripts/contact.js
+++ scripts/contact.js
@@ -1,15 +1,5 @@
-const form = document.querySelector("#contactForm");
-const fName = document.querySelector("fullName");
-const fNameError = document.querySelector("fullNameError");
-
-const email = document.querySelector("email");
-const emailError = document.querySelector("#emailError");
-
-const subject = document.querySelector("#subject");
-const subjectError = document.querySelector("#subjectError");
-
-const message = document.querySelector("#message");
-const messageError = document.querySelector("#messageError");
-
+function validForm(){
+    alert("alert")
+}
 
 
--- scripts/slidecarousell.js
+++ scripts/slidecarousell.js
@@ -1,34 +1,57 @@
-const url ="https://www.flowerp.online/wp-json/wp/v2/posts";
-const display = document.querySelector(".carousel-slider");
-const container = document.querySelector(".slider");
 
+/*api fetch*/
 
-
-
+const url = "https://www.flowerp.online/wp-json/wp/v2/posts";
 fetch(url)
-    .then(response => response.json())
-    .then(data => posts(data))
-    .catch(error => {
-      
-      display.innerHTML = "Page not found"
-    })
-   
+.then(response => response.json())
+.then(data => {
+    listPosts(data);
+})
+.catch((error) => {
+    console.error('Error:', error);
+});
 
-post = (posts) => {
-  console.log(posts);
+const output = document.querySelector(".slider");
+function listPosts (posts) {
+    let mySlides = "";
+    for (let post of posts){
+        console.log(post);
+        mySlides += `
+        <div class = "carousel-slider">
+       
+            
+            <h3>${post.title.rendered}</h3>
+            <p>${post.content.rendered}</p>
 
-      
-      for (post of posts) {
-          
-        
-          let newPost = `
-          <div class "postAll">
-          <h3>${post.content}</h3>
-          </div>  
-         
-          `;
-          display.innerHTML += newPost;
-          
-        }
-      }
-    
+        </div>
+        `;
+
+    }
+    output.innerHTML = mySlides;
+}
+
+
+
+
+
+/*carousel*/
+var sliderPost = 1;
+showPost(sliderPost);
+
+function plusSlides(n) {
+  showPost(sliderPost += n);
+}
+
+function showPost(n) {
+  var i;
+  var slider = document.querySelectorAll(".carousel-slider");
+  if (n > slider.length) {sliderPost = 1}
+  if (n < 1) {sliderPost = slider.length}
+  for (i = 0; i < slider.length; i++){
+    slider[i].style.display = "none";
+  }
+  
+  slider[sliderPost-1].style.display = "block";
+}
+
+
